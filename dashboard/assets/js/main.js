60e2d54c4a2e07342f6e3ab99eabed1bf0134498
Maxinjector <official.ayoadekola@gmail.com>
2024-05-14 14:07:09 +0100

    dashboard

---
+++ dashboard/assets/js/main.js
@@ -0,0 +1,25 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+
+menuBtn.addEventListener("click", () => {
+	sideMenu.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+	sideMenu.style.display = "none";
+});
+
+function transactions(evt, name) {
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	document.getElementById(name).style.display = "block";
+	evt.currentTarget.className += " active";
+}
